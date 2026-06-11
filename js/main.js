4e8e88fb150f5497b5db37c77ae168db89e5a11d
Nathaly Alvarez <alvarezquinteronathaly@gmail.com>
2026-06-11 11:23:20 -0600

    Se realizo el cambio de navbar al archivo js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+document.getElementById("menu").innerHTML = `
+ <nav class="navbar py-2 px-5 d-flex justify-content-between">
+        <div class="d-flex align-items-center">
+            <a href="index.html" style="text-decoration: none;">
+                <h1 class="text-logo" style="font-size: 2.5em; font-weight: bold;">
+                    E-TECH MÉXICO
+                </h1>
+            </a>
+            <a href="index.html"><img src="./assets/images/logo_e_tech_transparente.png" alt="logo tech"
+                    style="height: 100px; width: auto;"></a>
+        </div>
+
+        <div class="d-flex gap-3 justify-content-center align-items-center">
+            <a class="btn btn-primary" href="index.html" role="button">Inicio</a>
+            <a class="btn btn-primary" href="nosotros.html" role="button">Nosotros</a>
+            <a class="btn btn-primary" href="catalogo.html" role="button">Catalago</a>
+            <a class="btn btn-primary" href="login.html" role="button">Login</a>
+            <a class="btn btn-primary" href="nosotros.html" role="button">Nosotros</a>
+            <a class="btn btn-primary" href="categorias.html" role="button">Categorías </a>
+
+        </div>
+    </nav>
+
+`;
